12dfd40ef5b921306c7b97c9669b8f6bd0a46360
tomaszlach86 <tomaszlach86@gmail.com>
2019-05-21 05:32:33 +0200

    added main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(Quarter.I);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj parametr X:");
+        double first = sc.nextDouble();
+        System.out.println("Podaj parametr Y:");
+        double second = sc.nextDouble();
+
+        Point p = new Point(first, second);
+
+        System.out.println(PointCalculator.getQuarter(p));
+
     }
 }
